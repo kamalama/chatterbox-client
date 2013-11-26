93dc50d21eb96d11083be8f7df32cd82f9d90652
Hack Reactor Students <students@hackreactor.com>
2013-11-25 19:49:06 -0800

    messages update automatically, and refactored code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,12 @@
 // YOUR CODE HERE:
+//ajax call to parse server to fetch messages
 var getMessage = $.ajax({
   // always use this url
   url: 'https://api.parse.com/1/classes/chatterbox',
   type: 'GET',
   contentType: 'application/json',
+  //once messages are returned from the server, runs displayMessages, otherwise
+  //responseJSON can't be called on the undefined getMessages object
   success: function (data) {
     displayMessages();
   },
@@ -13,16 +16,23 @@ var getMessage = $.ajax({
 });
 
 
-var parsedMessages = function() {
-  return getMessage.responseJSON.results;
-};
-
-
 var displayMessages = function() {
+  //save parsed messages into a usable array
+  var messages = getMessage.responseJSON.results;
   // iterate over the object with responses
-  var messages = parsedMessages();
-  for (var i = 0; i < messages.length; i++){
-    $(".messages").append(Remove.js("<li>" + messages[i].text + "</li>"));
+  //if there are no messages, prepend the messages to the DOM
+  if($("li").length === 0) {
+    for (var i = 0; i < 30; i++){
+      $(".messages").prepend(Remove.js("<li>" + messages[i].username + ': '
+        + messages[i].text + " sent at: " + messages[i].createdAt + "</li>"));
+    }
+    //otherwise, update existing elements
+  } else {
+    for (var i = 0; i < 30; i++){
+      $( "ul li:nth-last-child("+ i +")" ).replaceWith(Remove.js("<li>" + messages[i].username + ': '
+          + messages[i].text + " sent at: " + messages[i].createdAt + "</li>"));
+    }
   }
 };
-
+//runs displayMessages every 5 seconds to get any new messages
+setInterval(displayMessages, 5000);
